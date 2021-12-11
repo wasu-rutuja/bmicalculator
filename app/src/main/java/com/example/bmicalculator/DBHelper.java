f437e537e933b90194948320bbeebb51d0cc8318
Rutuja <wasurutuja20@gmail.com>
2021-12-11 15:15:12 +0530

    with working sql lite

--- app/src/main/java/com/example/bmicalculator/DBHelper.java
+++ app/src/main/java/com/example/bmicalculator/DBHelper.java
@@ -1,8 +1,12 @@
 package com.example.bmicalculator;
-
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper( Context context) {
@@ -10,12 +14,44 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table Userdetails(date String ,age INT,height INT,weight INT,bmi FLOAT)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists Userdetails");
     }
+
+    public Boolean insert(int age,int weight,double height,double bmi){
+        SQLiteDatabase DB=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        Date currentTime = Calendar.getInstance().getTime();
+       // android.text.format.DateFormat df = new android.text.format.DateFormat();
+        Calendar c = Calendar.getInstance();
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH);
+       int year = c.get(Calendar.YEAR);
+       String date = day + "/" + (month+1) + "/" + year;
+        contentValues.put("date","11/11/2020");
+        contentValues.put("age",age);
+        contentValues.put("height",height);
+        contentValues.put("weight",weight);
+        contentValues.put("bmi",bmi);
+        long result=DB.insert("Userdetails",null,contentValues);
+        if(result==-1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public Cursor view() {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor3 = DB.rawQuery("select * from Userdetails ", null);
+        return  cursor3;
+    }
+
 }
+
--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bmicalculator;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,16 +10,19 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-    android.widget.Button mcalculatebmi;
+    android.widget.Button mcalculatebmi,mviewRecord;
     TextView mcurrentheight;
     TextView mcurrentage, mcurrentweight;
     ImageView mincrementage,mincrementweight,mdecrementage,mdecrementweight;
     SeekBar mseekbarforheight;
     RelativeLayout mmale,mfemale;
+
+    DBHelper dbh=new DBHelper(this);
 
     int intweight=55;
     int intage=22;
@@ -47,7 +51,32 @@ public class MainActivity extends AppCompatActivity {
         mseekbarforheight=findViewById(R.id.seekbarforheight);
         mmale=findViewById(R.id.male);
         mfemale=findViewById(R.id.female);
+        mviewRecord=findViewById(R.id.viewRecord);
 
+
+        mviewRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = dbh.view();
+                if (cursor.getCount() == 0) {
+                    Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                }
+                StringBuffer str = new StringBuffer();
+                while (cursor.moveToNext()) {
+                    str.append("Date : " + cursor.getString(0) + "\n");
+                    str.append("Age : " + cursor.getString(1) + "\n");
+                    str.append("Height : " + cursor.getString(2) + "m"+"\n");
+                    str.append("Weight : " + cursor.getString(3) + "\n");
+                    str.append("BMI : " + cursor.getString(4) + "\n");
+                    str.append(" "+"\n\n");
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("Past BMI Record");
+                builder.setMessage(str.toString());
+                builder.show();
+            }
+        });
         mmale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -129,23 +158,55 @@ public class MainActivity extends AppCompatActivity {
 
                 if(typeofuser.equals("0")){
                     Toast.makeText(getApplicationContext(),"Select Gender",Toast.LENGTH_SHORT).show();
+//                    View view1 = toast.getView();
+//
+//                    view1.getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+//                    TextView text = view.findViewById(android.R.id.message);
+//                    text.setTextColor(Color.BLACK);
+//                    toast.show();
+                    Intent intent =new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
                 }
                 else if(mintprogress.equals("0")){
                     Toast.makeText(getApplicationContext(),"Select Height",Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
                 }
                 else if(intage==0|| intage<0){
                     Toast.makeText(getApplicationContext(),"Incorrect Age",Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
                 }
                 else if(intweight<=0){
                     Toast.makeText(getApplicationContext(),"Incorrect Weight",Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
+
                 }
                 else{
+                    double height1=Integer.parseInt(mintprogress)/100.0;
+                    int weight1=Integer.parseInt(weight2);
+                    int age1=Integer.parseInt(age2);
+                    double bmi=weight1/(height1*height1);
+                    //String n = age2;
+                    //String m = weight2.getText().toString();
+                    //String r = r.getText().toString();
+
+                    boolean check = dbh.insert(age1, weight1,height1, bmi);
+                    if (check == true) {
+                        Toast.makeText(MainActivity.this, "Insertion Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insertion Failed", Toast.LENGTH_SHORT).show();
+                    }
+
+
                     Intent intent =new Intent(MainActivity.this,bmiactivity.class);
                     intent.putExtra("gender",typeofuser);
                     intent.putExtra("height",mintprogress);
                     intent.putExtra("age",age2);
                     intent.putExtra("weight",weight2);
                     startActivity(intent);
+
                 }
 
                 finish();
